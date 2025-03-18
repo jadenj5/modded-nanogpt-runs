f32b29495ea7cd351f22fef1e06166004ae10ba1
jadenjohnson <jaden8000@gmail.com>
2025-03-18 17:53:41 -0400

    Remove scaling on mask

--- train_gpt.py
+++ train_gpt.py
@@ -195,12 +195,12 @@ class Muon(torch.optim.Optimizer):
                     buf.lerp_(g, 1 - group["momentum"])
                     g = g.lerp_(buf, group["momentum"]) if group["nesterov"] else buf
 
-                    mask = (orig_sign == g.sign()).to(g.dtype)
+                    mask = (orig_sign == g.sign())
                     g.mul_(mask)
-                    dim_x = g.numel()
-                    nnz_x = torch.count_nonzero(g)
-                    scaling = dim_x / (nnz_x + 1)
-                    g.mul_(scaling)
+                    #dim_x = g.numel()
+                    #nnz_x = torch.count_nonzero(g)
+                    #scaling = dim_x / (nnz_x + 1)
+                    #g.mul_(scaling)
 
                     g = zeropower_via_newtonschulz5(g, steps=group["ns_steps"]).flatten()
                 else:
